63d654b3f6190999f2e0d52fffa9fd0826d00c0a
Karl Rapur <rapurkarl@gmail.com>
2021-03-15 19:48:07 +0200

    Added Table component.

---
+++ frontend/src/components/Table.vue
@@ -0,0 +1,24 @@
+<template>
+  <DataTable :value="feedbacks">
+    <Column field="id" header="#"></Column>
+    <Column field="name" header="Name"></Column>
+    <Column field="email" header="Email"></Column>
+    <Column field="category" header="Category"></Column>
+    <Column field="text" header="Text"></Column>
+  </DataTable>
+</template>
+
+<script>
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+export default {
+  name: "Table",
+  components: {
+    Column,
+    DataTable,
+  },
+  props: ["feedbacks"],
+};
+</script>
+
+<style scoped></style>
