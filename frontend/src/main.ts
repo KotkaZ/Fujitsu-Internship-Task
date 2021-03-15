bea951a56ecb668a1831ee8a6d9585b46aaffb22
Karl Rapur <rapurkarl@gmail.com>
2021-03-15 19:46:40 +0200

    Added PrimeVue support.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import "./plugins/axios";
 import { createApp } from "vue";
 import App from "./App.vue";
+import PrimeVue from "primevue/config";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
@@ -8,4 +9,5 @@ import store from "./store";
 createApp(App)
   .use(store)
   .use(router)
+  .use(PrimeVue)
   .mount("#app");
