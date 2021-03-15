cda8947e18fe0037f5ec90dbde1ecd2f584f3784
Karl Rapur <rapurkarl@gmail.com>
2021-03-15 19:47:22 +0200

    Fixed old vue2 syntax in axios conf.

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -1,6 +1,3 @@
-"use strict";
-
-import Vue from "vue";
 import axios from "axios";
 
 // Full config:  https://github.com/axios/axios#request-config
@@ -39,23 +36,4 @@ _axios.interceptors.response.use(
   }
 );
 
-Plugin.install = function(Vue, options) {
-  Vue.axios = _axios;
-  window.axios = _axios;
-  Object.defineProperties(Vue.prototype, {
-    axios: {
-      get() {
-        return _axios;
-      }
-    },
-    $axios: {
-      get() {
-        return _axios;
-      }
-    }
-  });
-};
-
-Vue.use(Plugin);
-
-export default Plugin;
+export default _axios;
