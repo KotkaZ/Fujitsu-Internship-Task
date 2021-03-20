80834983e5db37fae169b0b087f5e5782d2023ed
Karl Rapur <rapurkarl@gmail.com>
2021-03-20 15:49:50 +0200

    Implemeted basic store for feedbacks.

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -3,12 +3,13 @@ import axios from "axios";
 // Full config:  https://github.com/axios/axios#request-config
 // axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers.post["Content-Type"] =
+  "application/x-www-form-urlencoded";
 
 const config = {
-  // baseURL: process.env.baseURL || process.env.apiUrl || ""
-  // timeout: 60 * 1000, // Timeout
-  // withCredentials: true, // Check cross-site Access-Control
+  baseURL: process.env.baseURL || process.env.apiUrl || "",
+  timeout: 60 * 1000, // Timeout
+  withCredentials: true // Check cross-site Access-Control
 };
 
 const _axios = axios.create(config);
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,8 +1,25 @@
 import { createStore } from "vuex";
+import axios from "../plugins/axios";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    feedbacks: []
+  },
+  getters: {
+    getFeedbacks: state => state.feedbacks
+  },
+  mutations: {
+    setFeedbacks: (state, feedbacks) => (state.feedbacks = feedbacks)
+  },
+  actions: {
+    async fetchFeedbacks({ commit }) {
+      const feedbacks = await axios.get("feedbacks");
+      commit("setFeedbacks", feedbacks);
+    },
+    async sumbitFeedback({ dispatch }, feedback) {
+      await axios.post("feedbacks", feedback);
+      dispatch("fetchFeedbacks");
+    }
+  },
   modules: {}
 });
