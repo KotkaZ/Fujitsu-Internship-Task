34c59c19d9dc6642c19984398d8ae67f46282d60
Karl Rapur <rapurkarl@gmail.com>
2021-03-20 21:36:05 +0200

    Initial backend commit.

---
+++ backend/src/main/java/kotkaz/github/com/fujitsubackend/FujitsuBackendApplication.java
@@ -0,0 +1,13 @@
+package kotkaz.github.com.fujitsubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FujitsuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FujitsuBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/kotkaz/github/com/fujitsubackend/FujitsuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kotkaz.github.com.fujitsubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FujitsuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
